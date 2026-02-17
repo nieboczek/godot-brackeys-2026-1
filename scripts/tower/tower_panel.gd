03b557852df9009023185c6ef84f7c0ddd0d3587
nieboczek <bartekkoraliki@gmail.com>
2026-02-17 16:00:25 +0100

    pressing the cost of a tower will now start placing it

--- scripts/tower/tower_panel.gd
+++ scripts/tower/tower_panel.gd
@@ -7,11 +7,11 @@ signal bought(scene: PackedScene)
 @export var tower_scene: PackedScene
 @export var cost: int
 
-@onready var button: TextureButton = $VBoxContainer/TextureButton
+@onready var button: Button = $Button
 
 func _ready() -> void:
-	$VBoxContainer/RichTextLabel.text = "[img width=20]res://assets/blood.png[/img]%s" % cost
-	button.texture_normal = tower_texture
+	$Button/VBoxContainer/RichTextLabel.text = "[img width=20]res://assets/blood.png[/img]%s" % cost
+	$Button/VBoxContainer/TextureRect.texture = tower_texture
 	button.disabled = cost > Manager.blood
 	Manager.blood_changed.connect(_blood_changed)
 
@@ -19,6 +19,6 @@ func _ready() -> void:
 func _blood_changed(new_blood: int) -> void:
 	button.disabled = cost > new_blood
 
-func _on_texture_button_pressed() -> void:
+func _on_button_pressed() -> void:
 	Manager.blood -= cost
 	bought.emit(tower_scene)
